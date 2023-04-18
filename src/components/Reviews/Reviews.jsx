96c093b7513cb3f11bca51ec693ecdd2925f60b8
Connor Smith <connorsmith94@outlook.com>
2023-04-18 11:09:17 +0100

    add single review page

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,34 +1,36 @@
 import { getReviews } from "../../api";
 import { Header } from "../Header/Header";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export const Reviews = () => {
     
     const [reviews, setReviews] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-
-      const fetchReviews = async () => {
+    
+    const fetchReviews = async () => {
         setIsLoading(true);
         const reviews = await getReviews()
             setReviews(reviews);
             setIsLoading(false);
       }
-
+      
       useEffect(() => {
-        fetchReviews();
-      }, []);
-
-
-    return (
-
-        <main>
+          fetchReviews();
+        }, []);
+        
+        
+        return (
+            
+            <main>
         <Header />
         <section>
         <h2 className="review-list-title">Reviews</h2>
         <ul className="review-list">
             {isLoading ? <li>Loading</li> : reviews.map(review => {
                 return <li key={review.review_id}>
-                    <h3>{review.title}</h3>
+                    <h3><Link to={`/reviews/${review.review_id}`}>{review.title}</Link></h3>
+                    <img src={review.review_img_url} alt={review.title}/>
                     <h4>{review.owner}</h4>
                     <div>
                     <p>Designer: {review.designer}</p>
