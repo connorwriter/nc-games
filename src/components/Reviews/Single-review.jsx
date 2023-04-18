64074a50de075fc8c2aae2bcbf7877ca47921449
Connor Smith <connorsmith94@outlook.com>
2023-04-18 16:09:35 +0100

    add upvote button and functionality

--- src/components/Reviews/Single-review.jsx
+++ src/components/Reviews/Single-review.jsx
@@ -1,12 +1,18 @@
-import { getReviewById } from "../../api";
+import { getReviewById, patchReviewVote } from "../../api";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Header } from "../Header/Header";
 import { Comments } from "../Comments/Comments";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
+
 
 export const SingleReview = (props) => {
     const [review, setReview] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [voteDisabled, setVoteDisabled] = useState(false);
+    const [error, setError] = useState(null);
+    const [votes, setVotes] = useState(review.votes)
 
     const formattedDate = new Date(review.created_at).toDateString();
     const {review_id} = useParams();
@@ -15,18 +21,40 @@ export const SingleReview = (props) => {
         setIsLoading(true);
         const review = await getReviewById(id)
             setReview(review);
+            setVotes(review.votes);
             setIsLoading(false);
+          
+      }
+
+      const updateReviewVote = async (id, increment) => {
+        try {
+
+          const review = await patchReviewVote(id, increment)
+          setReview(review)
+        }
+        catch (error){
+          setError(error)
+        }
+      }
+
+      const handleClick = () => {
+        updateReviewVote(review_id, 1);
+        setVoteDisabled(true);
+        setVotes(votes + 1)
       }
 
       useEffect(() => {
         fetchReview(review_id);
       }, []);
 
+      if (error) {
+        return <p>{error}</p>;
+      }
       return (
         
         <main>
         <Header />
-          {isLoading ? <p>Loading</p> : 
+        {isLoading ? <p>Loading</p> : 
         <section className="review">
           <div className="review-flex">
         <p className="review-category">{review.category}</p>
@@ -38,10 +66,11 @@ export const SingleReview = (props) => {
         </div>
         <h2>{review.title}</h2>
         <img src={review.review_img_url} alt={review.title} />
-        <p className="votes">votes: {review.votes}</p>
+        <p className="votes"><span>votes: {votes}</span> <button disabled={voteDisabled} onClick={handleClick}className="upvote-btn"><FontAwesomeIcon className="upvote" icon={faChevronUp} /></button></p>
         <p className="review-body">{review.review_body}</p>
       
         </section>}
+        
         <section className="comments">
           <Comments review_id={review_id}/>
         </section>
